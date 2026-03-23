c8298ebada03092d2728aa9cfc0b9a68849b8081
Utkarsh Chaudhari <utkarsh410@gmail.com>
2026-03-23 20:17:15 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, Float, String
+from .database import Base
 
+class Transaction(Base):
+
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True)
+
+    amount = Column(Float)
+
+    score = Column(Float)
+
+    risk = Column(String)
