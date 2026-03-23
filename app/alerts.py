3d0a59b356beec65650844d9420e50150f1b0631
Utkarsh Chaudhari <utkarsh410@gmail.com>
2026-03-23 20:14:59 +0530

    Update alerts.py

--- app/alerts.py
+++ app/alerts.py
@@ -1,1 +1,8 @@
+def generate_alert(score, risk):
 
+    if risk in ["CRITICAL", "HIGH"]:
+        return {
+            "alert" : True,
+            "message" : f"Fraud has been detected with score {score}"
+        }
+    return {"alert": False}
