59c28474fd3bc0ba2dd47293e434ca18b480738e
Utkarsh Chaudhari <utkarsh410@gmail.com>
2026-06-06 08:37:00 +0530

    Delete app directory

--- app/alerts.py
+++
@@ -1,8 +0,0 @@
-def generate_alert(score, risk):
-
-    if risk in ["CRITICAL", "HIGH"]:
-        return {
-            "alert" : True,
-            "message" : f"Fraud has been detected with score {score}"
-        }
-    return {"alert": False}
